f0d866009d99f87922f86aeb758450107a2c1446
Alexander Moshnitsky <106654532+pitonoff@users.noreply.github.com>
2022-08-26 11:33:28 +0400

    init: created a repo

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# task_box
+# Momentum
+---
+## Overview
+[Momentum app](https://jo-soyoung.github.io/momentum/) made with Vanilla JS
+
+## Features
+* [x] Clock
+* [x] Username persistance
+* [x] To do list
+* [x] Random background image
+* [x] Weather with geolocation
+* [x] Responsive web
---
+++ weather.js
@@ -0,0 +1,57 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEYS = "af73c63e23979876d77b4f987238b274";
+const COORDS = "coords";
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEYS}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `За окном: ${temperature} C, мы находимся в: ${place}`;
+    });
+}
+
+function saveCoords(coordsObj) {
+  localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSuccess(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+  const coordsObj = {
+    latitude,
+    longitude,
+  };
+  saveCoords(coordsObj);
+  getWeather(latitude, longitude);
+}
+
+function handleGeoError() {
+  console.log("Cannot access geo location");
+}
+
+function askForCoords() {
+  navigator.geolocation.getCurrentPosition(handleGeoSuccess, handleGeoError);
+}
+
+function loadCoords() {
+  const loadedCoords = localStorage.getItem(COORDS);
+  if (loadedCoords === null) {
+    askForCoords();
+  } else {
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude, parsedCoords.longitude);
+  }
+}
+
+function init() {
+  loadCoords();
+}
+
+init();
